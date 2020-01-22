13bf80f0e5eb15e55d4324fb8e287412c8a692e2
Test <masteritua@gmail.com>
2020-01-22 23:07:51 +0200

    lesson11

--- Group/models.py
+++ Group/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from faker import Faker
+from Teacher.models import Teacher
+from Student.models import Student
 
 
 class Group(models.Model):
@@ -7,9 +9,19 @@ class Group(models.Model):
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     email = models.CharField(max_length=20)
+    teacher_id = models.ForeignKey(
+        Teacher, blank=True, null=True, on_delete=models.CASCADE)
+    student_id = models.ForeignKey(
+        Student, blank=True, null=True, on_delete=models.CASCADE)
 
     class Meta:
         db_table = "group"
+
+    def get_curator(self):
+        return f'{self.teacher_id.first_name} {self.teacher_id.last_name}  {self.teacher_id.email}'
+
+    def get_leader_class(self):
+        return f'{self.student_id.first_name} {self.student_id.last_name}  {self.student_id.email}'
 
     def get_info(self):
         return f'{self.first_name} {self.last_name} {self.email}'
